ea5e3db40207199fc6aac965e1754beab263f7ac
dustming <40034156+dustming@users.noreply.github.com>
2019-11-10 00:15:03 -0500

    Update AddLakeandObs.py

--- AddLakeandObs.py
+++ AddLakeandObs.py
@@ -118,9 +118,9 @@ def Getbasinoutlet(ID,basin,fac,dir,nrows,ncols):
     else:
         crow = nrow 
         ccol = ncol 
+        ifound = 0
         for i in range(0,1000): #### find next 1000 grids, to find the basin outlet 
             nrow,ncol =  Nextcell(dir,crow,ccol)
-            ifound = 0
             if nrow < 0 or ncol < 0:
                 ifound = 1
                 break
@@ -137,9 +137,9 @@ def Getbasinoutlet(ID,basin,fac,dir,nrows,ncols):
                 crow = nrow
                 ccol = ncol
                 continue
-            if ifound == 0: 
-                arcpy.AddMessage(" true basin outlet not found for ID...."+ str(ID))
-            return nrow,ncol        
+        if ifound == 0: 
+            arcpy.AddMessage(" true basin outlet not found for ID...."+ str(ID))
+        return nrow,ncol        
 
 ##################################################################3
 
