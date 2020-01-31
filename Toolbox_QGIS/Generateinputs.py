04f80ce552e3162d5f46249c9d3cfefeb851214d
dustming <40034156+dustming@users.noreply.github.com>
2020-01-30 21:56:58 -0500

    Update Generateinputs.py

--- Toolbox_QGIS/Generateinputs.py
+++ Toolbox_QGIS/Generateinputs.py
@@ -1,6 +1,8 @@
 
 ###################################################################3
 def Defcat(out,outletid):
+    import numpy as np
+    import copy
     otsheds = np.full((1,1),outletid)
     Shedid = np.full((10000000,1),-99999999999999999)
     psid = 0
@@ -42,22 +44,90 @@ def writeraster(w_filname,nraster,dataset):
 
 def Generateinputdata_hydrosheds(hyshddem,hyshddir,hyshdacc,hyshdply,WidDep,Lakefile,Landuse,Landuseinfo,obspoint,OutHyID,OutHyID2,OutputFolder):
 
-### function to generate inputs that is needed for lake-river routing network delineatation 
-### Inputs    
-#hyshddem = sys.argv[1]    ## the path for hydroshed dem data 
-#hyshddir = sys.argv[2]    ## the path for hydroshed flow direction data 
-#hyshdacc = sys.argv[3]    ## the path for hydroshed flow accumulation data 
-#hyshdply = sys.argv[4]    ## the path for hydroshed level 12 catchments 
-#WidDep = sys.argv[5]      ## the path for channel bankfull width and depth Polyline
-#Lakefile = sys.argv[6]    ## the path for hydrolake polygon 
-#Landuse = sys.argv[7]     ## the path for landuse data 
-#Landuseinfo = sys.argv[8] ## the path for a csv table, which describe the flood plan mannning's coefficient of each land use type 
-#obspoint = sys.argv[9]    ## the path for the observation point shp file 
-#OutHyID = int(sys.argv[10]) ## the most downstream catchment hydroshed ID 
-#OutHyID2 = int(sys.argv[11]) ## the most upstream catchment hydroshed ID
-#OutputFolder = sys.argv[12] + "/"  ## the path for output folder to save processed files 
+    from qgis.core import (
+        QgsRasterLayer,
+        QgsVectorLayer,
+        QgsApplication,
+        QgsFeatureRequest,
+        QgsVectorFileWriter
+    )
+    from simpledbf import Dbf5
+    import os
+    import sys
+    import numpy as np
+    
+#    sys.path.append('C:/Users/dustm/Documents/GitHub/RoutingTool/Toolbox_QGIS/')
+    
+    OutHyID = int(OutHyID)
+    OutHyID2 = int(OutHyID2)
+    
+    QgsApplication.setPrefixPath("C:/QGIS310/apps/qgis", True)
+    Qgs = QgsApplication([],False)
+    Qgs.initQgis()
+    
+    r_dem_layer = QgsRasterLayer(hyshddem, "") ### load DEM raster as a  QGIS raster object to obtain attribute
+    if not r_dem_layer.isValid():
+        print("Layer failed to load!")
+            
+    cellSize = float(r_dem_layer.rasterUnitsPerPixelX())  ### Get Raster cell size
 
+    SptailRefid = r_dem_layer.crs().authid()   ### get Raster spatialReference id
+    
+    print("Working with a  sptail reference  :   " , r_dem_layer.crs().description(), "      ", SptailRefid)
+    print("The cell cize is   ",cellSize)
+    
+    if not os.path.exists(OutputFolder):
+        os.makedirs(OutputFolder)
+
+        
     hyinfocsv = hyshdply[:-3] + "dbf"
     VolThreshold = 0
-    print(hyinfocsv)
+    tempinfo = Dbf5(hyinfocsv)#np.genfromtxt(hyinfocsv,delimiter=',')
+    hyshdinfo = tempinfo.to_dataframe().values
+
+### obtain the HydroShed polygons that belongs to the target drainage area
+    HydroBasins1 = Defcat(hyshdinfo,OutHyID) ### return fid of polygons that needs to be select 
+
+### 
+    if OutHyID2 > 0:
+        HydroBasins2 = Defcat(hyshdinfo,OutHyID2)
+        
+###  exculde the Ids in HydroBasins2 from HydroBasins1
+        for i in range(len(HydroBasins2)):
+            rows =np.argwhere(HydroBasins1 == HydroBasins2[i])
+            HydroBasins1 = np.delete(HydroBasins1, rows)
+        
+        HydroBasins = HydroBasins1
+    else:
+        HydroBasins = HydroBasins1
+    
+### Load HydroSHED Layers 
+    hyshedl12 = QgsVectorLayer(hyshdply, "")
+    
+### Build qgis selection expression
+    where_clause = '"HYBAS_ID" IN'+ " ("
+    for i in range(0,len(HydroBasins)):
+        if i == 0:
+            where_clause = where_clause + str(HydroBasins[i])
+        else:
+            where_clause = where_clause + "," + str(HydroBasins[i])
+    where_clause = where_clause + ")"
+
+    req = QgsFeatureRequest().setFlags( QgsFeatureRequest.NoGeometry )
+    req.setFilterExpression(where_clause)
+    it = hyshedl12.getFeatures( req )
+    
+    ### obtain all feature id of selected polygons
+    selectedFeatureID = []
+    for feature in it:
+        selectedFeatureID.append(feature.id())
+        
+    hyshedl12.select(selectedFeatureID)   ### select with polygon id
+    
+    # Save selected polygons to output 
+    _writer = QgsVectorFileWriter.writeAsVectorFormat(hyshedl12, OutputFolder +"HyMask.shp", "UTF-8", hyshedl12.crs(), "ESRI Shapefile", onlySelected=True)
+
+#arcpy.AddMessage(HydroBasins)
+
+    Qgs.exitQgis()
     
