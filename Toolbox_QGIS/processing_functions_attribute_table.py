f044cd554edc604fd434a95b62e4e2580e625b5f
dustming <40034156+dustming@users.noreply.github.com>
2020-11-26 20:32:00 -0500

    Move New_SubId_To_Dissolve to processing_functions_attribute_table.py

--- Toolbox_QGIS/processing_functions_attribute_table.py
+++ Toolbox_QGIS/processing_functions_attribute_table.py
@@ -65,3 +65,94 @@ def Calculate_Longest_flowpath(mainriv_merg_info):
     Longestpath = max(longest_flow_pathes)
 
     return Longestpath
+    
+    
+    
+def New_SubId_To_Dissolve(subid,catchmentinfo,mapoldnew_info,upsubid = -1,ismodifids = -1,modifiidin = [-1],mainriv = [-1],Islake = -1,seg_order = -1):
+    sub_colnm = 'SubId'
+    routing_info      = catchmentinfo[['SubId','DowSubId']].astype('float').values
+    if ismodifids < 0:
+        Modify_subids1            = Defcat(routing_info,subid)   ### find all subids drainage to this subid
+        if upsubid > 0:
+            Modify_subids2        = Defcat(routing_info,upsubid)
+            mask = np.in1d(Modify_subids1, Modify_subids2)
+            Modify_subids = Modify_subids1[np.logical_not(mask)]
+        else:
+            Modify_subids =  Modify_subids1
+
+    else:
+        Modify_subids =  modifiidin
+#    print("##########################################")
+#    print(subid)
+#    print(Modify_subids)
+    cbranch                  = catchmentinfo[catchmentinfo[sub_colnm].isin(Modify_subids)].copy()
+    tarinfo                  = catchmentinfo[catchmentinfo[sub_colnm] == subid].copy()   ### define these subs attributes
+    ### average river slope info
+
+    mainriv_merg_info = mainriv.loc[mainriv['SubId'].isin(Modify_subids)].copy()
+    mainriv_merg_info = mainriv_merg_info.loc[mainriv_merg_info['RivLength'] > 0].copy()
+    idx = tarinfo.index[0]
+#    print(tarinfo.loc[idx,'BasArea'],"1")
+    if len(mainriv_merg_info) > 0:
+        tarinfo.loc[idx,'RivLength'] = np.sum(mainriv_merg_info['RivLength'].values)
+        tarinfo.loc[idx,'RivSlope']  = np.average(mainriv_merg_info['RivSlope'].values ,weights = mainriv_merg_info['RivLength'].values)
+        tarinfo.loc[idx,'FloodP_n']  = np.average(mainriv_merg_info['FloodP_n'].values ,weights = mainriv_merg_info['RivLength'].values)
+        tarinfo.loc[idx,'Q_Mean']    = np.average(mainriv_merg_info['Q_Mean'].values   ,weights = mainriv_merg_info['RivLength'].values)
+        tarinfo.loc[idx,'Ch_n']      = np.average(mainriv_merg_info['Ch_n'].values     ,weights = mainriv_merg_info['RivLength'].values)
+        tarinfo.loc[idx,'BkfWidth']  = np.max(mainriv_merg_info['BkfWidth'].values)
+        tarinfo.loc[idx,'BkfDepth']  = np.max(mainriv_merg_info['BkfDepth'].values)
+
+    tarinfo.loc[idx,'BasArea']       = np.sum(cbranch['BasArea'].values)
+#    tarinfo.loc[idx,'NonLDArea']     = np.sum(cbranch['NonLDArea'].values)
+    if len(cbranch) > 0:
+        tarinfo.loc[idx,'BasSlope']      = np.average(cbranch['BasSlope'].values,  weights = cbranch['BasArea'].values)
+        tarinfo.loc[idx,'MeanElev']      = np.average(cbranch['MeanElev'].values,  weights = cbranch['BasArea'].values)
+        tarinfo.loc[idx,'BasAspect']     = np.average(cbranch['BasAspect'].values, weights = cbranch['BasArea'].values)
+
+        tarinfo.loc[idx,'Max_DEM']       = np.max(cbranch['Max_DEM'].values)
+        tarinfo.loc[idx,'Min_DEM']       = np.min(cbranch['Min_DEM'].values)
+#    print(tarinfo.loc[idx,'BasArea'],"2")
+    if Islake == 1:   ## Meger subbasin covered by lakes, Keep lake outlet catchment  DA, stream order info
+        Longestpath = Calculate_Longest_flowpath(mainriv_merg_info)
+        tarinfo.loc[idx,'RivLength'] = Longestpath
+
+    elif Islake == 2:
+        tarinfo.loc[idx,'RivLength'] = 0.0
+        tarinfo.loc[idx,'IsLake']    = 2
+    elif Islake < 0:
+#        tarinfo.loc[idx,'Strahler']      = -1.2345
+#        tarinfo.loc[idx,'Seg_ID']        = -1.2345
+#        tarinfo.loc[idx,'Seg_order']     = -1.2345
+#        tarinfo.loc[idx,'DA']            = -1.2345
+        tarinfo.loc[idx,'HyLakeId']      = -1.2345
+        tarinfo.loc[idx,'LakeVol']       = -1.2345
+        tarinfo.loc[idx,'LakeArea']      = -1.2345
+        tarinfo.loc[idx,'LakeDepth']     = -1.2345
+        tarinfo.loc[idx,'Laketype']      = -1.2345
+        tarinfo.loc[idx,'IsLake']        = -1.2345
+
+    tarinfo.loc[idx,'centroid_x']    = -1.2345
+    tarinfo.loc[idx,'centroid_y']    = -1.2345
+
+    if seg_order >0 :
+        tarinfo.loc[idx,'Seg_order']      = seg_order
+
+    mask1 = mapoldnew_info['SubId'].isin(Modify_subids) ## catchment newly determined to be merged to target catchment
+    mask2 = mapoldnew_info['nsubid'] == subid ###for subbasin already processed to drainage into this target catchment
+    mask  = np.logical_or(mask1,mask2)
+
+
+    ### the old downsub id of the dissolved polygon is stored in DowSubId
+    for col in tarinfo.columns:
+        if col == 'SubId':
+#            print(tarinfo[col].values[0])
+            mapoldnew_info.loc[mask,'nsubid']     = tarinfo[col].values[0]
+#            print(mapoldnew_info.loc[mask,'nsubid'])
+        elif col == 'nsubid' or col == 'ndownsubid' or col == 'Old_SubId' or col == 'Old_DowSubId':
+            continue
+        else:
+            mapoldnew_info.loc[mask, col]         = tarinfo[col].values[0]
+#    print(mapoldnew_info.loc[mask1,['BasArea','nsubid','SubId']])
+#    print(mapoldnew_info.loc[mapoldnew_info['nsubid'] == subid,['BasArea','nsubid','SubId']])
+    return mapoldnew_info
+    
