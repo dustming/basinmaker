38fd2a06d11489c690c7bc14dd4188f84656f33f
dustming <40034156+dustming@users.noreply.github.com>
2020-11-30 22:47:45 -0500

    upload data and example script for version 1 routing product

---
+++ Toolbox_QGIS/Example/Working_Procedure_With_Version_1_Product/README.md
@@ -0,0 +1,8 @@
+Only qgis is needed for this script, after setup python qgis environment.
+
+```python 
+python Script_to_working_with_Version_1_Product.py 
+
+```
+
+  
---
+++ Toolbox_QGIS/Example/Working_Procedure_With_Version_1_Product/Script_to_working_with_Version_1_Product.py
@@ -0,0 +1,49 @@
+import pytest
+from ToolboxClass import LRRT
+import os
+
+
+#
+#BasinMaker_Folder = "C:/Users/dustm/Documents/GitHub/RoutingTool"
+        
+    ###Floder where store the inputs for tests function
+Routing_Product_Folder = '../../tests/testdata/Routing_product_V1/'
+HRU_Folder = os.path.join('../../tests/testdata','HRU')
+    
+
+###Folder where the output will be generated 
+Output_Folder = os.path.join('../../tests/testdata','test11')
+    
+RTtool=LRRT()
+
+
+# Generate HRU without considering lakes, 1 subbasin is 1 hru 
+# If want to define lake hru just change 
+# Path_Connect_Lake_ply = '#' to 
+# Path_Connect_Lake_ply = os.path.join(Routing_Product_Folder,'Con_Lake_Ply.shp')
+# 
+RTtool.GenerateHRUS(OutputFolder = Output_Folder,
+                    Path_Subbasin_Ply =  os.path.join(Routing_Product_Folder,'finalcat_info.shp'),
+     				Path_Connect_Lake_ply = '#', #os.path.join(Routing_Product_Folder,'Con_Lake_Ply.shp'),
+					Path_Non_Connect_Lake_ply = '#',
+                    Path_Landuse_Ply = '#',Landuse_ID = 'Landuse_ID',
+                    Path_Soil_Ply = '#',Soil_ID = 'Soil_ID',
+                    Path_Veg_Ply = '#',Veg_ID = 'Veg_ID',
+                    Path_Other_Ply_1='#', Other_Ply_ID_1='O_ID_1',
+                    Path_Other_Ply_2='#', Other_Ply_ID_2='O_ID_2',
+					Landuse_info=os.path.join(HRU_Folder,'landuse_info.csv'),
+					Soil_info=os.path.join(HRU_Folder,'soil_info.csv'),
+					Veg_info=os.path.join(HRU_Folder,'veg_info.csv'),
+					DEM = os.path.join(Routing_Product_Folder,'Liever_DEM.tif')) 
+# define raven input                     
+RTtool.GenerateRavenInput(Path_final_hru_info = os.path.join(Output_Folder,'finalcat_hru_info.shp'), 
+                          OutputFolder = Output_Folder,
+                          Model_Name   = 'Mytest_model',
+                          WriteObsrvt  = False,
+#                          Startyear    = 2010, 
+#                          EndYear      = 2014,
+#                          CA_HYDAT     = CA_HYDAT,
+                          Old_Product  = True
+#                          WarmUp       = 1
+                          ) 
+                                       
