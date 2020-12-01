9c206305c0e687e0fec6f6003e7577ee6121afbb
dustming <40034156+dustming@users.noreply.github.com>
2020-12-01 07:47:11 -0500

    remove tab to sapce

--- Toolbox_QGIS/Example/Working_Procedure_With_Version_1_Product/Script_to_working_with_Version_1_Product.py
+++ Toolbox_QGIS/Example/Working_Procedure_With_Version_1_Product/Script_to_working_with_Version_1_Product.py
@@ -17,24 +17,24 @@ Output_Folder = Routing_Product_Folder #os.path.join('../../tests/testdata','tes
 RTtool=LRRT()
 
 
-# Generate HRU without considering lakes, 1 subbasin is 1 hru 
+# Generate HRU 
 # Connected lake polygon comes form hydrolake, needs to be extracted first 
 # it is not provied in routing product 
 #
 RTtool.GenerateHRUS(OutputFolder = Output_Folder,
                     Path_Subbasin_Ply =  os.path.join(Routing_Product_Folder,'finalcat_info.shp'),
      				Path_Connect_Lake_ply = os.path.join(Routing_Product_Folder,'Con_Lake_Ply.shp'),
-					Path_Non_Connect_Lake_ply = '#',
+                    Path_Non_Connect_Lake_ply = '#',
                     Path_Landuse_Ply = '#',Landuse_ID = 'Landuse_ID',
                     Path_Soil_Ply = '#',Soil_ID = 'Soil_ID',
                     Path_Veg_Ply = '#',Veg_ID = 'Veg_ID',
                     Path_Other_Ply_1='#', Other_Ply_ID_1='O_ID_1',
                     Path_Other_Ply_2='#', Other_Ply_ID_2='O_ID_2',
-					Landuse_info=os.path.join(HRU_Folder,'landuse_info.csv'),
-					Soil_info=os.path.join(HRU_Folder,'soil_info.csv'),
-					Veg_info=os.path.join(HRU_Folder,'veg_info.csv'),
-					DEM = os.path.join(Routing_Product_Folder,'Liever_DEM.tif')) 
-# define raven input                     
+                    Landuse_info=os.path.join(HRU_Folder,'landuse_info.csv'),
+                    Soil_info=os.path.join(HRU_Folder,'soil_info.csv'),
+                    Veg_info=os.path.join(HRU_Folder,'veg_info.csv'),
+                    DEM = os.path.join(Routing_Product_Folder,'Liever_DEM.tif')) 
+# define raven input, using output of generathru,'os.path.join(Output_Folder,'finalcat_hru_info.shp')'                     
 RTtool.GenerateRavenInput(Path_final_hru_info = os.path.join(Output_Folder,'finalcat_hru_info.shp'), 
                           OutputFolder = Output_Folder,
                           Model_Name   = 'Mytest_model',
