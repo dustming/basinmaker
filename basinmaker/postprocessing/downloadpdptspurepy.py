32d25bf56e386f570eb1340a95e51904fc6cd081
dustming <dustming@gmail.com>
2022-04-17 19:24:51 -0400

    Add option to download data using lat and lon

---
+++ basinmaker/postprocessing/downloadpdptspurepy.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import geopandas
+from basinmaker.postprocessing.downloadpd import Download_Routing_Product_For_One_Gauge
+import os
+
+def Download_Routing_Product_From_Points_Or_LatLon(product_name,Lat = [-1],Lon = [-1]):
+    
+    data = pd.DataFrame(
+    {
+     'Latitude': Lat,
+     'Longitude': Lon,
+     }
+     )
+    
+    data_gpd = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(data.Longitude, data.Latitude))
+    data_gpd.crs = "EPSG:4326"
+    
+    if product_name == 'OLRP':
+        version = 'v1-0'
+        Drainage_region = geopandas.read_file("C:/Users/dustm/Downloads/test/OLRRP_drainage_region3.geojson")
+    
+        data_dr = geopandas.overlay(data_gpd, Drainage_region, how='identity')
+        if len(data_dr) >= 1:
+
+            dr = data_dr['Region'].values[0]
+            Subid,product_path = Download_Routing_Product_For_One_Gauge(gauge_name='#',product_name = product_name,region=dr,subreg = '#')
+            product_ply_path = os.path.join(product_path,'catchment_without_merging_lakes_'+version+'.shp')
+            product = geopandas.read_file(product_ply_path)
+            data_gpd = data_gpd.to_crs(product.crs)
+            data_pt = geopandas.overlay(data_gpd, product, how='identity')
+            Subid = data_pt['SubId'].values[0]
+            
+            return Subid,product_path 
+
+        else:
+            print("The point did not overlay with the routing product")
+            return -1,-1
+            
+    return -1, -1 
