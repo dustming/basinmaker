8d5da614e70d1020c99de5bed3d185a90dba965f
dustming <dustming@gmail.com>
2022-05-09 11:55:47 -0400

    support any drainage area thresthold

--- basinmaker/func/pdtable.py
+++ basinmaker/func/pdtable.py
@@ -1788,7 +1788,15 @@ def Change_Attribute_Values_For_Catchments_Need_To_Be_Merged_By_Increase_DA(
     mapoldnew_info["nsubid"] = -1
     mapoldnew_info["ndownsubid"] = -1
     mapoldnew_info.reset_index(drop=True, inplace=True)
-
+    max_da = np.max(mapoldnew_info['DrainArea'].values)
+    
+    ## stupid larege drainage area are provied
+    if Area_Min * 1000 * 1000 > max_da:
+        Area_Min = max_da/1000.0/1000.0 - 1
+        mapoldnew_info[Gauge_col_Name] =  -1
+        finalriv_info[Gauge_col_Name] = -1
+        
+         
     # select catchment segment that meet the drainage area
     Selected_riv = finalriv_info.loc[
         finalriv_info[DA_colnm] >= Area_Min * 1000 * 1000
