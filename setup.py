64896c194459c8a97e023c29a6e504216f29656f
dustming <dustming@gmail.com>
2022-05-19 14:25:28 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="basinmaker", 
-    version="2.3.0",
+    version="3.0.0",
     author="basinmaker development team",
     author_email="m43han@uwaterloo.ca",
     description="An automated GIS toolbox for watershed delineation with lakes",
