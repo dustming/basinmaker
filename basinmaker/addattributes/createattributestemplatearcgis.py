Fixed DA_Diff calculation and export nan for missing DA_Diff values
DA_Diff = (DA_Diff - DA_Obs) / DA_Obs * 100% in percentage. DA_Obs missing values in subbasin layer is set as -1.2345, and the resultant DA_Diff is set as nan.
DA_Diff in subbasin layer is set to be string type.

basinmaker/addattributes/createattributestemplatearcgis.py
@@ -326,8 +326,14 @@ def create_catchments_attributes_template_table(
     attri_table = calculate_bkf_width_depth(attri_table)
 
     attri_table = update_non_connected_catchment_info(attri_table)
-    attri_table.loc[attri_table['DA_Obs'] > 0,'DA_Diff'] = (attri_table.loc[attri_table['DA_Obs'] > 0,'DrainArea']/1000/1000 - attri_table.loc[attri_table['DA_Obs'] > 0,'DA_Obs'])/attri_table.loc[attri_table['DA_Obs'] > 0,'DrainArea']/1000/1000
-    attri_table.loc[attri_table['DA_Obs'] <= 0,'DA_Diff'] = -1.2345
+    attri_table.loc[attri_table['DA_Obs'] > 0,'DA_Diff'] = (attri_table.loc[attri_table['DA_Obs'] > 0,'DrainArea']/1000/1000 - attri_table.loc[attri_table['DA_Obs'] > 0,'DA_Obs'])/attri_table.loc[attri_table['DA_Obs'] > 0,'DA_Obs']
+    attri_table.loc[attri_table['DA_Obs'] <= 0,'DA_Diff'] = -999
+    # non-null values are shown as percentage
+    attri_table['DA_Diff'] = attri_table['DA_Diff'].apply(lambda x: f"{x*100:.2f}%" if x != -999 else "nan")
+
+    attri_table.loc[attri_table['DA_Obs'] <= 0, 'DA_Obs'] = -1.2345
+    # print(attri_table['DA_Diff'].values)
+    
     attri_table.loc[attri_table['RivLength'] == -1.2345,'RivSlope'] = -1.2345
     attri_table.loc[attri_table['RivLength'] == -1.2345,'FloodP_n'] = -1.2345
     attri_table.loc[attri_table['RivLength'] == -1.2345,'Max_DEM'] = -1.2345
@@ -376,8 +382,16 @@ def create_catchments_attributes_template_table(
     ]]
      
     cat_ply= cat_ply[['SubId','SHAPE']]
+    cat_ply['SubId']  = pd.to_numeric(cat_ply['SubId'])
+    cat_ply['SubId']  = cat_ply['SubId'].fillna(0)
+    cat_ply['SubId']  = cat_ply['SubId'].astype(int)
+
     cat_ply = cat_ply.merge(attri_table,on='SubId',how='left')
-    cat_ply = cat_ply.fillna(-1.2345)
+    for col_i in cat_ply.columns:
+        # DA_Diff use np.nan for null values
+        if col_i != "DA_Diff":
+            cat_ply[col_i] = cat_ply[col_i].fillna(-1.2345)
+
     cat_ply['Obs_NM'] = cat_ply['Obs_NM'].astype('str')
     cat_ply['SRC_obs'] = cat_ply['SRC_obs'].astype('str')
     cat_ply = cat_ply.drop_duplicates(subset=['SubId'], keep='first')
@@ -385,8 +399,16 @@ def create_catchments_attributes_template_table(
     cat_ply.spatial.to_featureclass(location=os.path.join(output_folder,catchment_without_merging_lakes+"_v1-0"),overwrite=True,sanitize_columns=False)
 
     riv_line= riv_line[['SubId','SHAPE']]
+    riv_line['SubId']  = pd.to_numeric(riv_line['SubId'])
+    riv_line['SubId']  = riv_line['SubId'].fillna(0)
+    riv_line['SubId']  = riv_line['SubId'].astype(int)
+
     riv_line = riv_line.merge(attri_table,on='SubId',how='left')
-    riv_line = riv_line.fillna(-1.2345)
+    for col_i in riv_line.columns:
+        # DA_Diff use np.nan for null values
+        if col_i != "DA_Diff":
+            riv_line[col_i] = riv_line[col_i].fillna(-1.2345)
+
     riv_line['Obs_NM'] = riv_line['Obs_NM'].astype('str')
     riv_line['SRC_obs'] = riv_line['SRC_obs'].astype('str')
     riv_line = riv_line.drop_duplicates(subset=['SubId'], keep='first')