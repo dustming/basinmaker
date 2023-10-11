6a16a309ae4a62556aa047fcdfae048cacceb2b9
dustming <dustming@gmail.com>
2023-10-11 19:38:24 -0400

    to extract ollrp product in one function

--- basinmaker/postprocessing/downloadpd.py
+++ basinmaker/postprocessing/downloadpd.py
@@ -5,66 +5,78 @@ import pandas as pd
 import numpy as np
 import gdown
 # define function two download routing product for a given gauge
-def Download_Routing_Product_For_One_Gauge(product_name,gauge_name = "#",region='#',subreg = '#'):
+
+
+def Download_Routing_Product_For_One_Gauge(product_name, gauge_name="#", region='#', subreg='#', version='v1-0'):
     if product_name == 'NALRP':
         version = 'v2-1'
         if gauge_name != '#':
-            gauge_info = pd.read_csv("https://github.com/dustming/RoutingTool/wiki/Files/obs_gauges_NA_v2-1.csv")
+            gauge_info = pd.read_csv(
+                "https://github.com/dustming/RoutingTool/wiki/Files/obs_gauges_NA_v2-1.csv")
             gauge_info_sl = gauge_info[gauge_info['Obs_NM'] == gauge_name]
             if len(gauge_info_sl) < 1:
-                print("The gauge ",gauge_name,"is not included in the routing product ")
+                print("The gauge ", gauge_name,
+                      "is not included in the routing product ")
                 SubId = -1
                 product_path = '#'
             else:
                 if gauge_info_sl['Use_region'].values[0] < 1:
-#                    print(gauge_info_sl)
-#                    print(gauge_info_sl['Region'].values[0],gauge_info_sl['Sub_Reg'].values[0])
+                    #                    print(gauge_info_sl)
+                    #                    print(gauge_info_sl['Region'].values[0],gauge_info_sl['Sub_Reg'].values[0])
                     region_id = int(gauge_info_sl['Region'].values[0])
                     subreg_id = int(gauge_info_sl['Sub_Reg'].values[0])
-                    url = "http://hydrology.uwaterloo.ca/basinmaker/data/original/drainage_region_%s_%s_v2-1.zip" % (str(region_id).zfill(4),str(subreg_id).zfill(5))
+                    url = "http://hydrology.uwaterloo.ca/basinmaker/data/original/drainage_region_%s_%s_v2-1.zip" % (
+                        str(region_id).zfill(4), str(subreg_id).zfill(5))
                     wget.download(url)
-                    os.system('unzip drainage_region_%s_%s_%s.zip' % (str(region_id).zfill(4),str(subreg_id).zfill(5),version))
-                    SubId =  gauge_info_sl['SubId'].values[0]
-                    product_name = "drainage_region_%s_%s_%s" % (str(region_id).zfill(4),str(subreg_id).zfill(5),version)
-                    product_path = os.path.join(os.getcwd(),product_name)
-                    print("The needed product locates at:",product_path)
-                    print("The Subbasin Id of the interested gauge is:",SubId)
+                    os.system('unzip drainage_region_%s_%s_%s.zip' % (
+                        str(region_id).zfill(4), str(subreg_id).zfill(5), version))
+                    SubId = gauge_info_sl['SubId'].values[0]
+                    product_name = "drainage_region_%s_%s_%s" % (
+                        str(region_id).zfill(4), str(subreg_id).zfill(5), version)
+                    product_path = os.path.join(os.getcwd(), product_name)
+                    print("The needed product locates at:", product_path)
+                    print("The Subbasin Id of the interested gauge is:", SubId)
                 else:
-#                    print(gauge_info_sl)
-#                    print(gauge_info_sl['Region'].values[0],gauge_info_sl['Sub_Reg'].values[0])
+                    #                    print(gauge_info_sl)
+                    #                    print(gauge_info_sl['Region'].values[0],gauge_info_sl['Sub_Reg'].values[0])
                     region_id = int(gauge_info_sl['Region'].values[0])
 #                    subreg_id = int(gauge_info_sl['Sub_Reg'].values[0])
-                    url = "http://hydrology.uwaterloo.ca/basinmaker/data/original/drainage_region_%s_v2-1.zip" % (str(region_id).zfill(4))
+                    url = "http://hydrology.uwaterloo.ca/basinmaker/data/original/drainage_region_%s_v2-1.zip" % (
+                        str(region_id).zfill(4))
                     wget.download(url)
-                    os.system('unzip drainage_region_%s_v2-1.zip' % (str(region_id).zfill(4)))
-                    SubId =  gauge_info_sl['SubId'].values[0]
-                    product_name = "drainage_region_%s_v2-1" % (str(region_id).zfill(4))
-                    product_path = os.path.join(os.getcwd(),product_name)
-                    print("The needed product locates at:",product_path)
-                    print("The Subbasin Id of the interested gauge is:",SubId)
-
+                    os.system('unzip drainage_region_%s_v2-1.zip' %
+                              (str(region_id).zfill(4)))
+                    SubId = gauge_info_sl['SubId'].values[0]
+                    product_name = "drainage_region_%s_v2-1" % (
+                        str(region_id).zfill(4))
+                    product_path = os.path.join(os.getcwd(), product_name)
+                    print("The needed product locates at:", product_path)
+                    print("The Subbasin Id of the interested gauge is:", SubId)
 
     if product_name == 'CLRH':
         version = 'v1-0'
         if gauge_name != '#':
 
-            url = "http://hydrology.uwaterloo.ca/CLRH/data/%s.zip" % (gauge_name)
+            url = "http://hydrology.uwaterloo.ca/CLRH/data/%s.zip" % (
+                gauge_name)
             wget.download(url)
             product_name = "%s" % (gauge_name)
-            product_path = os.path.join(os.getcwd(),product_name)
-            os.system('unzip %s.zip -d  %s' % (gauge_name,product_path))
+            product_path = os.path.join(os.getcwd(), product_name)
+            os.system('unzip %s.zip -d  %s' % (gauge_name, product_path))
             # print('unzip %s.zip - d  %s' % (gauge_name,product_path))
-            print("The needed product locates at:",product_path)
+            print("The needed product locates at:", product_path)
             SubId = -1
 
-    if product_name == 'OLRP':
-        version = 'v1-0'
-        gauge_info = pd.read_csv("https://github.com/dustming/RoutingTool/wiki/Files/OIH_gauge_info.csv")
-        productinfo = pd.read_csv("https://github.com/dustming/RoutingTool/wiki/Files/OLRRP.csv")
+    if product_name == 'OLRP' or product_name == 'OLLRP':
+        gauge_info = pd.read_csv(
+            "https://github.com/dustming/RoutingTool/wiki/Files/OIH_gauge_info.csv")
+        productinfo = pd.read_csv(
+            "https://github.com/dustming/RoutingTool/wiki/Files/OLRRP.csv")
         if gauge_name != '#':
             gauge_info_sl = gauge_info[gauge_info['Obs_NM'] == gauge_name]
             if len(gauge_info_sl) < 1 or gauge_info_sl['SubId'].values[0] < 0:
-                print("The gauge ",gauge_name,"is not included in the routing product ")
+                print("The gauge ", gauge_name,
+                      "is not included in the routing product ")
                 SubId = -1
                 product_path = '#'
             else:
@@ -73,62 +85,70 @@ def Download_Routing_Product_For_One_Gauge(product_name,gauge_name = "#",region=
                     subreg_id = gauge_info_sl['Sub_Reg'].values[0]
                     mask1 = productinfo['Region'] == region_id
                     mask2 = productinfo['Sub_Reg'] == subreg_id
-                    mask = np.logical_and(mask1,mask2)
-                    url_veiw = productinfo.loc[mask,'Download'].values[0]
+                    mask = np.logical_and(mask1, mask2)
+                    url_veiw = productinfo.loc[mask, 'Download'].values[0]
 
                     url_veiw = url_veiw.split("/")
-                    url = "https://drive.google.com/u/0/uc?id=%s&export=download"%(url_veiw[5])
-                    output = 'drainage_region_%s_%s_%s.zip' % (region_id,subreg_id,version)
+                    url = "https://drive.google.com/u/0/uc?id=%s&export=download" % (
+                        url_veiw[5])
+                    output = 'drainage_region_%s_%s_%s.zip' % (
+                        region_id, subreg_id, version)
 
-                    os.system('gdown %s -O %s' % (url_veiw[5],output))
+                    os.system('gdown %s -O %s' % (url_veiw[5], output))
 
-                    os.system('unzip drainage_region_%s_%s_%s.zip' % (region_id,subreg_id,version))
-                    SubId =  gauge_info_sl['SubId'].values[0]
-                    product_name = 'drainage_region_%s_%s_%s' % (region_id,subreg_id,version)
-                    product_path = os.path.join(os.getcwd(),product_name)
-                    print("The needed product locates at:",product_path)
-                    print("The Subbasin Id of the interested gauge is:",SubId)
+                    os.system('unzip drainage_region_%s_%s_%s.zip' %
+                              (region_id, subreg_id, version))
+                    SubId = gauge_info_sl['SubId'].values[0]
+                    product_name = 'drainage_region_%s_%s_%s' % (
+                        region_id, subreg_id, version)
+                    product_path = os.path.join(os.getcwd(), product_name)
+                    print("The needed product locates at:", product_path)
+                    print("The Subbasin Id of the interested gauge is:", SubId)
                 else:
                     region_id = gauge_info_sl['Region'].values[0]
                     subreg_id = '-'
                     mask1 = productinfo['Region'] == region_id
                     mask2 = productinfo['Sub_Reg'] == subreg_id
-                    mask = np.logical_and(mask1,mask2)
-                    url_veiw = productinfo.loc[mask,'Download'].values[0]
+                    mask = np.logical_and(mask1, mask2)
+                    url_veiw = productinfo.loc[mask, 'Download'].values[0]
                     url_veiw = url_veiw.split("/")
-                    url = "https://drive.google.com/u/0/uc?id=%s&export=download"%(url_veiw[5])
-                    output = 'drainage_region_%s_%s.zip' % (region_id,version)
+                    url = "https://drive.google.com/u/0/uc?id=%s&export=download" % (
+                        url_veiw[5])
+                    output = 'drainage_region_%s_%s.zip' % (region_id, version)
 
-                    os.system('gdown %s -O %s' % (url_veiw[5],output))
+                    os.system('gdown %s -O %s' % (url_veiw[5], output))
 
-                    os.system('unzip drainage_region_%s_%s.zip' % (region_id,version))
-                    SubId =  gauge_info_sl['SubId'].values[0]
-                    product_name ='drainage_region_%s_%s' % (region_id,version)
-                    product_path = os.path.join(os.getcwd(),product_name)
-                    print("The needed product locates at:",product_path)
-                    print("The Subbasin Id of the interested gauge is:",SubId)
+                    os.system('unzip drainage_region_%s_%s.zip' %
+                              (region_id, version))
+                    SubId = gauge_info_sl['SubId'].values[0]
+                    product_name = 'drainage_region_%s_%s' % (
+                        region_id, version)
+                    product_path = os.path.join(os.getcwd(), product_name)
+                    print("The needed product locates at:", product_path)
+                    print("The Subbasin Id of the interested gauge is:", SubId)
 
-        elif region != '#' and subreg =='#':
+        elif region != '#' and subreg == '#':
             region_id = region
             subreg_id = '-'
             mask1 = productinfo['Region'] == region_id
             mask2 = productinfo['Sub_Reg'] == subreg_id
-            mask = np.logical_and(mask1,mask2)
-            url_veiw = productinfo.loc[mask,'Download'].values[0]
+            mask = np.logical_and(mask1, mask2)
+            url_veiw = productinfo.loc[mask, 'Download'].values[0]
             url_veiw = url_veiw.split("/")
-            url = "https://drive.google.com/u/0/uc?id=%s&export=download"%(url_veiw[5])
-            output = 'drainage_region_%s_%s.zip' % (region_id,version)
+            url = "https://drive.google.com/u/0/uc?id=%s&export=download" % (
+                url_veiw[5])
+            output = 'drainage_region_%s_%s.zip' % (region_id, version)
 
-            os.system('gdown %s -O %s' % (url_veiw[5],output))
+            os.system('gdown %s -O %s' % (url_veiw[5], output))
 
-            os.system('unzip drainage_region_%s_%s.zip' % (region_id,version))
-            SubId =  -1
-            product_name ='drainage_region_%s_%s' % (region_id,version)
-            product_path = os.path.join(os.getcwd(),product_name)
+            os.system('unzip drainage_region_%s_%s.zip' % (region_id, version))
+            SubId = -1
+            product_name = 'drainage_region_%s_%s' % (region_id, version)
+            product_path = os.path.join(os.getcwd(), product_name)
 
-        elif region != '#' and subreg !='#':
+        elif region != '#' and subreg != '#':
             print("todo")
         else:
             print("Wrong option provided")
 
-    return SubId,product_path
+    return SubId, product_path
--- basinmaker/postprocessing/downloadpdptspurepy.py
+++ basinmaker/postprocessing/downloadpdptspurepy.py
@@ -2,63 +2,147 @@ import pandas as pd
 import geopandas
 from basinmaker.postprocessing.downloadpd import Download_Routing_Product_For_One_Gauge
 import os
-import numpy as np 
+import numpy as np
+from basinmaker.postprocessing.selectprodpurepy import Select_Routing_product_based_SubId_purepy
 
-def Download_Routing_Product_From_Points_Or_LatLon(product_name,Lat = [-1],Lon = [-1]):
-    
+
+def Download_Routing_Product_From_Points_Or_LatLon(product_name, Lat=[-1], Lon=[-1]):
+
     data = pd.DataFrame(
-    {
-     'Latitude': Lat,
-     'Longitude': Lon,
-     }
-     )
-    
-    data_gpd = geopandas.GeoDataFrame(data, geometry=geopandas.points_from_xy(data.Longitude, data.Latitude))
+        {
+            'Latitude': Lat,
+            'Longitude': Lon,
+        }
+    )
+
+    data_gpd = geopandas.GeoDataFrame(
+        data, geometry=geopandas.points_from_xy(data.Longitude, data.Latitude))
     data_gpd.crs = "EPSG:4326"
-    
+
     if product_name == 'OLRP':
         version = 'v1-0'
-        Drainage_region = geopandas.read_file("https://github.com/dustming/RoutingTool/wiki/Files/OLRRP_drainage_region.geojson")
-    
+        Drainage_region = geopandas.read_file(
+            "https://github.com/dustming/RoutingTool/wiki/Files/OLRRP_drainage_region.geojson")
+
         data_dr = geopandas.overlay(data_gpd, Drainage_region, how='identity')
         if len(data_dr) >= 1 and data_dr['Region'].values[0] in np.unique(Drainage_region['Region'].values):
 
             dr = data_dr['Region'].values[0]
-            Subid,product_path = Download_Routing_Product_For_One_Gauge(gauge_name='#',product_name = product_name,region=dr,subreg = '#')
-            product_ply_path = os.path.join(product_path,'catchment_without_merging_lakes_'+version+'.shp')
+            Subid, product_path = Download_Routing_Product_For_One_Gauge(
+                gauge_name='#', product_name=product_name, region=dr, subreg='#')
+            product_ply_path = os.path.join(
+                product_path, 'catchment_without_merging_lakes_'+version+'.shp')
             product = geopandas.read_file(product_ply_path)
             data_gpd = data_gpd.to_crs(product.crs)
             data_pt = geopandas.overlay(data_gpd, product, how='identity')
             Subid = data_pt['SubId'].values[0]
-            print("The needed product locates at:",product_path)
-            print("The Subbasin Id of the lat, lon is:",Subid)
-            return Subid,product_path 
+            print("The needed product locates at:", product_path)
+            print("The Subbasin Id of the lat, lon is:", Subid)
+            return Subid, product_path
 
         else:
             print("The point did not overlay with the routing product")
-            return -1,-1
+            return -1, -1
 
     if product_name == 'NALRP':
         version = 'v2-1'
-        Drainage_region = geopandas.read_file("https://github.com/dustming/RoutingTool/wiki/Files/NA_drainage_region.geojson")
-    
+        Drainage_region = geopandas.read_file(
+            "https://github.com/dustming/RoutingTool/wiki/Files/NA_drainage_region.geojson")
+
         data_dr = geopandas.overlay(data_gpd, Drainage_region, how='identity')
         if len(data_dr) >= 1:
 
             dr = data_dr['Region'].values[0]
-            Subid,product_path = Download_Routing_Product_For_One_Gauge(gauge_name='#',product_name = product_name,region=dr,subreg = '#')
-            product_ply_path = os.path.join(product_path,'catchment_without_merging_lakes_'+version+'.shp')
+            Subid, product_path = Download_Routing_Product_For_One_Gauge(
+                gauge_name='#', product_name=product_name, region=dr, subreg='#')
+            product_ply_path = os.path.join(
+                product_path, 'catchment_without_merging_lakes_'+version+'.shp')
             product = geopandas.read_file(product_ply_path)
             data_gpd = data_gpd.to_crs(product.crs)
             data_pt = geopandas.overlay(data_gpd, product, how='identity')
             Subid = data_pt['SubId'].values[0]
-            print("The needed product locates at:",product_path)
-            print("The Subbasin Id of the lat, lon is:",Subid)
-            return Subid,product_path 
+            print("The needed product locates at:", product_path)
+            print("The Subbasin Id of the lat, lon is:", Subid)
+            return Subid, product_path
 
         else:
             print("The point did not overlay with the routing product")
-            return -1,-1
-            
-                        
-    return -1, -1 
+            return -1, -1
+
+    return -1, -1
+
+
+def Extract_Routing_Product(version='v1-0', by='Gauge_ID', gauge_id='#', subid=-1, lat=-1, lon=-1, output_path='#'):
+    SubId = -1  # default value
+    product_path = '#'
+    if by == 'Gauge_ID':
+        SubId, product_path = Download_Routing_Product_For_One_Gauge(
+            gauge_name=gauge_id, product_name='OLLRP', region='#', subreg='#', version=version)
+    elif by == 'LatLon':
+        data = pd.DataFrame(
+            {
+                'Latitude': lat,
+                'Longitude': lon,
+            }
+        )
+        data_gpd = geopandas.GeoDataFrame(
+            data, geometry=geopandas.points_from_xy(data.Longitude, data.Latitude))
+        data_gpd.crs = "EPSG:4326"
+
+        Drainage_region = geopandas.read_file(
+            "https://github.com/dustming/RoutingTool/wiki/Files/OLRRP_drainage_region.geojson")
+
+        data_dr = geopandas.overlay(data_gpd, Drainage_region, how='identity')
+        if len(data_dr) >= 1 and data_dr['Region'].values[0] in np.unique(Drainage_region['Region'].values):
+            dr = data_dr['Region'].values[0]
+            SubId, product_path = Download_Routing_Product_For_One_Gauge(
+                gauge_name='#', product_name='OLLRP', region=dr, subreg='#')
+            product_ply_path = os.path.join(
+                product_path, 'catchment_without_merging_lakes_'+version+'.shp')
+            product = geopandas.read_file(product_ply_path)
+            data_gpd = data_gpd.to_crs(product.crs)
+            data_pt = geopandas.overlay(data_gpd, product, how='identity')
+            SubId = data_pt['SubId'].values[0]
+
+        else:
+            print("The point did not overlay with the routing product")
+    else:
+        print("The input by is not supported")
+
+    if SubId == -1 or product_path == '#':
+        return
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    # Search for the file
+    foldername = find_file(
+        product_path, 'catchment_without_merging_lakes_'+version+'.shp')
+
+    Select_Routing_product_based_SubId_purepy(
+        OutputFolder=output_path,
+        Routing_Product_Folder=foldername,
+        mostdownid=[SubId],
+        mostupstreamid=[-1],
+    )
+
+    from basinmaker.postprocessing.postprocessingfunctions import (
+        combine_catchments_covered_by_the_same_lake_method,
+    )
+
+    combine_catchments_covered_by_the_same_lake_method(
+        Routing_Product_Folder=output_path,
+        qgis_prefix_path='#',
+        area_thresthold=3,
+        length_thresthold=0,
+        gis_platform="purepy",
+    )
+
+# Function to search for the target file
+
+
+def find_file(directory, target_file):
+    for foldername, subfolders, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename == target_file:
+                return foldername
